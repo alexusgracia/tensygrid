78a1a9dafc916811e9aa442a9c37edacee395600
Alexandre Gràcia <alexusgracia@users.noreply.github.com>
2026-02-03 18:27:17 +0100

    it works but meh

--- src/main.py
+++ src/main.py
@@ -1,18 +1,22 @@
 from cpn_builder import CPNBuilder
+from cpn_linearizer import CPNLinearizer
 
-# Equations (strings)
 equations1 = [
-    '2*x + y - x*u',
-    'x - 3*y + z - u*z',
-    '4*z - u',
-]
-
-equations2 = [
     '-dx1 - x1 + u',
     '-dx2 - x2 + x1*x2',
 ]
 
-equations = equations2
+equations2 = [
+    '-dx1 - x1 + u + 2*x2',
+    '-dx2 - x2 + x1*x2',
+]
+
+equations3 = [
+    '-3*dx1 + 2*x1 - 5*u',
+    '-dx2 - x2 + x1*x2',
+]
+
+equations = equations3
 
 # Build CPN representation
 builder = CPNBuilder(equations)
@@ -20,6 +24,19 @@ builder = CPNBuilder(equations)
 
 builder.print_info()
 
+
+# Define parameters
+n_states = 2
+m_inputs = 1
+p_outputs = 0
+q_equations = n_states
+N_signals = 2 * n_states + m_inputs + p_outputs
+
 # Extract matrices S and P
 S = builder.get_S()
 P = builder.get_P()
+
+linearizer = CPNLinearizer(equations, S, P, n_states, m_inputs, p_outputs, q_equations, N_signals)
+
+
+linearizer.linearize(test_zeros = False, debug = False, total_time = False, print_eigenvalues = True)
