6e83b6a8fd1a0b3c154bbf92a1e2439c5f65a0c6
alexusgracia <alexusgracia@users.noreply.github.com>
2026-02-09 15:20:52 +0100

    test

--- code/main.py
+++ code/main.py
@@ -1,12 +1,13 @@
 from MatrixBuilder import MatrixBuilder
 import sympy as sp
 
-equations = [
-    '-dx1 - x1 + u+ 2x2',
-    '-dx2 - x2 + x1*x2',
+equations_sets = [
+    ['-dx1 - x1 + u','-dx2 - x2 + x1*x2',],
+    ['-dx1 - x1 + u + 2*x2','-dx2 - x2 + x1*x2',],
+    ['-3*dx1 + 2*x1 - 5*u','-dx2 - x2 + x1*x2',]
 ]
 
-matrix = MatrixBuilder(equations)
+matrix = MatrixBuilder(equations_sets[1])
 matrix.print_equations()
 
 #print("S =")
