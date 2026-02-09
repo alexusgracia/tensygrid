973462df10b7b263fa1abc275a4e1eea60c0fb40
Carlos Herrera Vazquez <84268470+Lentejas1@users.noreply.github.com>
2026-02-09 16:11:02 +0100

    I added formatting proposals to be studied

--- code/main.py
+++ code/main.py
@@ -13,3 +13,22 @@ matrix.print_equations()
 #sp.pprint(builder.S) 
 #print("P =")
 #sp.pprint(builder.P)
+
+
+### FORMATTING PROPOSAL ###
+from sympy.parsing.latex import parse_latex
+from sympy.parsing.sympy_parser import parse_expr, standard_transformations, implicit_multiplication_application
+expr = r"-\frac{dx}{dt} - x1 + u+ 2x2"
+transformations = (standard_transformations + (implicit_multiplication_application,))
+
+equations = [
+    '-dx1 - x1 + u+ 2x2',
+    parse_latex(expr),
+    sp.parse_expr("-dx1 - x1 + u+ 2x2", transformations=transformations),
+    '-dx2 - x2 + x1*x2',
+
+]
+
+matrix = MatrixBuilder(equations)
+matrix.print_equations()
+
